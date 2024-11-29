ccb4f39473efcda64fc4a1865825f3d19bc1b15c
koheiitokazu <kohei.itokazu077@myci.csuci.edu>
2024-11-28 23:52:12 -0800

    setup geometry

--- cloth-simulation/src/main.js
+++ cloth-simulation/src/main.js
@@ -1,24 +1,45 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// scene, camera, renderer
+const scene = new THREE.Scene()
 
-setupCounter(document.querySelector('#counter'))
+const camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight, 0.1,1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene,camera);
+
+// shapes
+const geometry = new THREE.CylinderGeometry(5,5,20,32,32,true)
+const material = new THREE.MeshStandardMaterial ({color:0xFF6347});
+const cylinder = new THREE.Mesh(geometry,material);
+
+scene.add(cylinder)
+
+// lighting
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight)
+
+// helper functions
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(gridHelper)
+
+// controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate()
