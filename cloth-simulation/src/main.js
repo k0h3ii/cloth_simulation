9fb68f65c768ce1cc753500a07fe8f4bdad008d6
koheiitokazu <kohei.itokazu077@myci.csuci.edu>
2024-11-30 20:53:56 -0800

    color picker and toggle wireframe

--- cloth-simulation/src/main.js
+++ cloth-simulation/src/main.js
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
 import * as CANNON from 'cannon-es';
+import { Sphere } from 'cannon-es';
 
 // Physics world
 const world = new CANNON.World({
@@ -46,18 +47,39 @@ const groundMesh = new THREE.Mesh(groundGeo, groundMat);
 scene.add(groundMesh);
 
 // Cylinder
-const geometry = new THREE.CylinderGeometry(5, 5, 20, 32, 1, true);
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const geometry = new THREE.CylinderGeometry(5, 5, 20, 32, 20, true);
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347, side: THREE.DoubleSide});
 const cylinder = new THREE.Mesh(geometry, material);
 cylinder.position.y = 10; // Lift cylinder above ground
 scene.add(cylinder);
 
+var wireGeo = new THREE.WireframeGeometry(geometry);
+var wireMat = new THREE.LineBasicMaterial({color: 0xffffff});
+var wireframe = new THREE.LineSegments(wireGeo,wireMat);
+wireframe.position.y = 10;
+scene.add(wireframe);
+
+// wireframe.visible = false;
 // Helpers
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(gridHelper);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
+
+// GUI
+const gui = new dat.GUI();
+const options = {
+  cylinderColor: '#ff6347',
+  wireframe: true
+};
+
+gui.addColor(options,'cylinderColor').onChange(function(e){
+  cylinder.material.color.set(e);
+});
+gui.add(options,'wireframe').onChange(function(e){
+  wireframe.visible = e;
+});
 
 // Animation loop
 function animate() {
